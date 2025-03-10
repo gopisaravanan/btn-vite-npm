bf0b6d534fd591ef1a7dcd369bfc00afe56a4cfb
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-10 14:14:39 +0530

    tailwindcss classname issue fix

--- lib/Button.tsx
+++ lib/Button.tsx
@@ -8,8 +8,8 @@ export interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button
-    // className="px-s4 py-2 bg-violet-600 text-white border-none rounded-md cursor-pointer"
-    className="button"
+    className="px-s4 py-2 bg-violet-600 text-white border-none rounded-md cursor-pointer"
+    // className="button"
     onClick={onClick}
     >
       {children}
