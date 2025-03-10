c3230771511cd965480d484b8965a835ac79510d
Gopi saravanan R <gopi.saravanan@kaaylabs.com>
2025-03-10 13:31:39 +0530

    external css and tsconfig fix

--- lib/Button.tsx
+++ lib/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Button.css'
 export interface ButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
@@ -8,16 +9,7 @@ const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button
     // className="px-s4 py-2 bg-violet-600 text-white border-none rounded-md cursor-pointer"
-    style={{
-      padding: "8px 16px",
-      backgroundColor: "#7c3aed", // Violet-600
-      color: "#ffffff",
-      border: "none",
-      borderRadius: "6px",
-      cursor: "pointer",
-      fontSize: "16px",
-      fontWeight: "500",
-    }}
+    className="button"
     onClick={onClick}
     >
       {children}
